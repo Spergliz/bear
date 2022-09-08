02267515c9e909e18db63ad31eadf68600b531fd
liz <97650879+Spergliz@users.noreply.github.com>
2022-09-07 20:20:07 -0600

    Update main.js

--- main.js
+++ main.js
@@ -10,12 +10,7 @@ function searchClicked() {
   name = name.toLowerCase();
 
   // Test Input Variable and update the page
-  if (name === 'po' || name === 'dragon warrior') {
-    // Update page to Po
-    document.getElementById('character-name').innerHTML = 'Po';
-    document.getElementById('main-img').src = 'img/po.png';
-    document.getElementById('quote').innerHTML =
-      'Buddy, I am the Dragon Warrior.';
+  
   } else if (name === 'tigress' || name === 'master tigress') {
     // Update page to Tigress
     document.getElementById('character-name').innerHTML = 'Master Tigress';
@@ -60,4 +55,12 @@ function searchClicked() {
     document.getElementById('main-img').src = 'img/question-mark.png';
     document.getElementById('quote').innerHTML = 'Character Not Found';
   }
-}
+
+function ChangeCharacter (){
+  if (name === 'po' || name === 'dragon warrior') {
+    // Update page to Po
+    document.getElementById('character-name').innerHTML = 'Po';
+    document.getElementById('main-img').src = 'img/po.png';
+    document.getElementById('quote').innerHTML =
+      'Buddy, I am the Dragon Warrior.';
+}}
