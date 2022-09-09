0b8a08a31215f35e222cbf9a3875d46914c900e3
liz <97650879+Spergliz@users.noreply.github.com>
2022-09-08 22:36:43 -0600

    Update main.js

--- main.js
+++ main.js
@@ -12,44 +12,26 @@ function searchClicked() {
   // Test Input Variable and update the page
   if (name === 'po'|| name === 'dragon warrior') { change('Po','po','buddy')
   } else if (name === 'tigress' || name === 'master tigress') {
-    change('tigress','tigress','That was pretty hardcore!'  )
+    change('Master Tigress','tigress','That was pretty hardcore!'  )
   } else if (name === 'mantis' || name === 'master mantis') {
     change('Master Mantis','mantis','Fear the bug!')
   } else if (name === 'monkey' || name === 'master monkey') {
     change('Monkey','monkey','We should hang out')
   } else if (name === 'crane' || name === 'master crane') {
-   change('Master Crane','crane')
-    // Update page to Crane
-   // document.getElementById('character-name').innerHTML = 'Master Crane';
-   // document.getElementById('main-img').src = 'img/crane.png';
-   // document.getElementById('quote').innerHTML ='You can chain my body, but you will never chain my warrior spirit!';
+   change('Master Crane','crane','You can chain my body, but you will never chain my warrior spirit!')
   } else if (name === 'viper' || name === 'master viper') {
-    // Update page to Viper
-    document.getElementById('character-name').innerHTML = 'Master Viper';
-    document.getElementById('main-img').src = 'img/viper.png';
-    document.getElementById('quote').innerHTML =
-      "I don't need to bite to fight!";
+    change('Master viper','viper','Id dont need to bite to fight!')
   } else if (name === 'shifu' || name === 'master shifu') {
-    // Update page to Master Shifu
-    document.getElementById('character-name').innerHTML = 'Master Shifu';
-    document.getElementById('main-img').src = 'img/shifu.png';
-    document.getElementById('quote').innerHTML = 'There is now a Level Zero.';
+    change('Master Shifu','shifu','There is now a Level Zero.')
   } else if (name === 'ping' || name === 'mr. ping') {
-    // Update page to Mr. Ping
-    document.getElementById('character-name').innerHTML = 'Mr. Ping';
-    document.getElementById('main-img').src = 'img/mr-ping.png';
-    document.getElementById('quote').innerHTML =
-      'We are noodle folk, broth runs through our veins!';
+    change('Mr.ping','mr-ping','We are noodle folk, broth runs through our veins!')
   } else {
-    // Update page to Question Mark
-    document.getElementById('character-name').innerHTML = '?????';
-    document.getElementById('main-img').src = 'img/question-mark.png';
-    document.getElementById('quote').innerHTML = 'Character Not Found';
-  }
+    change('?????','question-mark.png','character not found')
+}
 }
 function change (name,imgname,quote){
-    // Update page to Po
     document.getElementById('character-name').innerHTML = name;
     document.getElementById('main-img').src = `img/${imgname}.png`;
     document.getElementById('quote').innerHTML =quote;
 }
+//god have mercy on me my hands hurt, i need to add a clipboard to ubuntu baddly 
