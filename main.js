fa6d5b1c637011e7e83e7a1b816fabc0de05457d
liz <97650879+Spergliz@users.noreply.github.com>
2022-09-08 19:34:15 -0600

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ function searchClicked() {
   name = name.toLowerCase();
 
   // Test Input Variable and update the page
-  if (name === 'po'|| name === 'dragon warrior') { change('Po','po','buddy00')
+  if (name === 'po'|| name === 'dragon warrior') 
+  { change('Po','po','buddy00')
   } else if (name === 'tigress' || name === 'master tigress') {
     change('tigress','tigress','That was pretty hardcore!'
   } else if (name === 'mantis' || name === 'master mantis') {
